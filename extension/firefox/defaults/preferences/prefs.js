76df05d4f7b2bb749970d30cbbf50c1b72b380ce
John Turner <groups@norgg.org>
2010-06-15 18:41:12 +0100

    Starts of firefox plugin.

---
+++ extension/firefox/defaults/preferences/prefs.js
@@ -0,0 +1,6 @@
+pref("extensions.charting.boolpref", false);
+pref("extensions.charting.intpref", 0);
+pref("extensions.charting.stringpref", "A string");
+
+// https://developer.mozilla.org/en/Localizing_extension_descriptions
+pref("extensions.charting@mhnltd.co.uk.description", "chrome://charting/locale/overlay.properties");
