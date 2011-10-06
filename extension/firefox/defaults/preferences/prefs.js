6444d0f491f79c766ef02e5ee5736406f75e3bac
John Turner <j.turner@mhnltd.co.uk>
2011-10-06 13:57:34 +0100

    Point firefox plugin to main charting site by default.

--- extension/firefox/defaults/preferences/prefs.js
+++ extension/firefox/defaults/preferences/prefs.js
@@ -1,6 +1,6 @@
 pref("extensions.charting.apiKey", "Please log in to get API key.");
 pref("extensions.charting.user", "Please log in to charting.");
-pref("extensions.charting.url", "http://charting-test.mhnltd.co.uk");
+pref("extensions.charting.url", "http://charting.mhnltd.co.uk");
 // pref("browser.preferences.instantApply", true);
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.charting@mhnltd.co.uk.description", "chrome://charting/locale/overlay.properties");
